1f163d61e3dd0d04d7e7ce058c4edc9778b1f75c
Anthony Perez <perezanthony1@ufl.edu>
2026-03-12 16:04:20 -0400

    admin dashboard dir created with filler page for me to work on

---
+++ app/adminDashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+//TODO: Create Club Form(s) For A/Each Club that will be able to add the name and slug to the DB (must verify attendee (the user) is an admin)
+export default function page() {
+  return (
+    <div><h1>Filler Message Hello Bro</h1></div>
+  )
+}
